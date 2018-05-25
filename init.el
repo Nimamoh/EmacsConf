f2088a2be76d2dad6cc37e81fe83a40df3a0ed79
Romain Gautier <rog@dreadnough.localdomain>
2018-05-25 17:33:34 +0200

    Change all the conf!

--- init.el
+++ init.el
@@ -1,39 +1,25 @@
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(require 'org)
+(require 'ob-tangle)
 
-(unless (package-installed-p 'spacemacs-theme)
-  (package-refresh-contents)
-  (package-install 'spacemacs-theme))
-
-
-;;;
-;; Loading the 'orged' configuration
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-;;;
-
-
+(setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
+(org-babel-load-file (expand-file-name "loader.org" init-dir))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (spacemacs-dark)))
- '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (yasnippet-snippets yasnippet company-irony pretty-mode expand-region mark-multiple swiper eterm-256color popup-kill-ring exwm-randr exwm exvm symon dmenu diminish spaceline company dashboard rainbow-delimiters sudo-edit hungry-delete switch-window rainbow-mode avy smex ido-vertical-mode org-bullets beacon spacemacs-theme which-key use-package))))
+    (magit to buffer-move zoom command-log-mode mode-icons dockerfile-mode clj-refactor cider clojure-snippets highlight-parentheses paredit-everywhere paredit ace-jump-mode ace-window yasnippet-snippets which-key use-package symon switch-window sudo-edit spacemacs-theme spaceline solarized-theme smex rainbow-delimiters pretty-mode popup-kill-ring org-bullets oceanic-theme nimbus-theme material-theme mark-multiple lsp-ui lsp-java ivy-hydra ido-vertical-mode hungry-delete google-translate exwm expand-region eterm-256color dmenu dimmer diminish dashboard counsel-projectile company-lsp company-irony color-theme-sanityinc-tomorrow beacon avy arjen-grey-theme)))
+ '(zoom-ignored-buffer-names (quote ("*command-log*")))
+ '(zoom-ignored-major-modes (quote (dired-mode markdown-mode)))
+ '(zoom-mode t nil (zoom))
+ '(zoom-size (quote (0.618 . 0.618))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 115 :width normal :foundry "UKWN" :family "Hack")))))
+ )
